3fae226e26a6e2ed56259281ae128ca56695848c
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-01-29 14:05:57 +0100

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get install conky
 chmod 777 *
-cp * /etc/conky/
-update-rc.d conky defaults
+cp * /etc/conky/ /etc/init.d/conky
+cp /usr/bin/conky 
 update-rc.d conky start
+update-rc.d conky defaults
+sudo /etc/init.d/conky restart
